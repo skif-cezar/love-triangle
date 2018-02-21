60ba457b39b51aacf153ff140c14dfe0e7c3bfff
skif-cezar <33913952+skif-cezar@users.noreply.github.com>
2018-02-21 23:00:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+   var count = 0;
+  var preferences.length = l;
+  for(var j = 0; j < l; j++){
+      if(j == (preferences[preferences[preferences[j]-1]-1]-1){
+          count++;
+          }
+  }
+  return count/3;
 };
